627606a7a750aa48bab9c2576eda84143c5d41c6
Chris Grimmett <chris@grimtech.net>
2017-09-12 17:01:19 -0700

    add joke.sh

---
+++ joke.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+curl \
+    -H "Accept: text/plain" \
+    --user-agent "asterisk-fortune https://github.com/insanity54/asterisk-fortune" \
+    https://icanhazdadjoke.com/
+
+
+
+exit 0
+
